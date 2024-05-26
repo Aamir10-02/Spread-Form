b8eaf495e0a3267cc6aaf6793addde40097983dd
Aamir10-02 <143529993+Aamir10-02@users.noreply.github.com>
2024-05-26 15:30:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,20 @@ import json
 import os
 
 app = Flask(__name__)
-app.secret_key = '' 
+app.secret_key = '1234' 
 
-# Path to the service account credentials file
-# CREDENTIALS_FILE = 'gs_credentials.json'
 credentials_info = json.loads(os.environ.get('GOOGLE_CREDENTIALS'))
-CREDENTIALS_FILE = 'your_credentials.json'
 
 # # Define the scope for the API
 SCOPES = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
-# # SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 
 # Load the credentials and create a client to interact with the Google Sheets API
 creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_info, SCOPES)
 client = gspread.authorize(creds)
 
 # The name of your Google Sheet
-SHEET_NAME = 'Add_your_spreadsheet'
+SHEET_NAME = 'Information'
 SHEET = client.open(SHEET_NAME)
 worksheet = SHEET.sheet
 
@@ -66,24 +62,3 @@ def display_info():
 if __name__ == '__main__':
     app.run(debug=True)
 
-# import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
-# from pprint import pprint
-
-
-
-# # Path to the service account credentials file
-# CREDENTIALS_FILE = 'gs_credentials.json'
-
-# # Define the scope for the API
-# SCOPES = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-
-# # SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-
-# # Load the credentials and create a client to interact with the Google Sheets API
-# creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, SCOPES)
-# client = gspread.authorize(creds)
-# sheet = client.open('Information').sheet1
-# data = sheet.get_all_records()
-# pprint(data)
-# sheet.delete_(6,1,"Aamir")
