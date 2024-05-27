259f9f449478a93398e9b5b190debe3682914150
Aamir10-02 <aamir2saiyad@gmail.com>
2024-05-27 12:46:06 +0100

    Update to use base64 encoded credentials from environment variable

--- app.py
+++ app.py
@@ -3,23 +3,29 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 import os
+import base64
+
 
 app = Flask(__name__)
 app.secret_key = '1234' 
 
-<<<<<<< HEAD
 # Path to the service account credentials file
 
-=======
->>>>>>> b8eaf495e0a3267cc6aaf6793addde40097983dd
-credentials_info = json.loads(os.environ.get('GOOGLE_CREDENTIALS'))
+# credentials_info = json.loads(os.environ.get('GOOGLE_CREDENTIALS'))
 
 # # Define the scope for the API
 SCOPES = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-
+credentials_base64 = os.getenv('GOOGLE_CREDENTIALS')
+if credentials_base64:
+    credentials_json = base64.b64decode(credentials_base64)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        json.loads(credentials_json), SCOPES
+    )
+else:
+    raise ValueError("GOOGLE_CREDENTIALS environment variable not set")
 
 # Load the credentials and create a client to interact with the Google Sheets API
-creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_info, SCOPES)
+# creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_info, SCOPES)
 client = gspread.authorize(creds)
 
 # The name of your Google Sheet
